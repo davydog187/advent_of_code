2f88ae0e2481f6c6f12d41d71af49a8919a756f8
Dave Lucia <davelucianyc@gmail.com>
2018-12-02 20:15:20 -0500

    day one

--- advent_elixir/lib/advent_elixir.ex
+++ advent_elixir/lib/advent_elixir.ex
@@ -4,21 +4,55 @@ defmodule AdventElixir do
   """
 
   @doc """
-  iex> day_1(["+1", "+1", "+1"])
+  iex> day_1a(["+1", "+1", "+1"])
   3
 
-  iex> day_1(["+1", "+1", "-2"])
+  iex> day_1a(["+1", "+1", "-2"])
   0
 
-  iex> day_1(["-1", "-2", "-3"])
+  iex> day_1a(["-1", "-2", "-3"])
   -6
   """
-  def day_1(frequencies) do
+  def day_1a(frequencies) do
     Enum.reduce(frequencies, 0, fn frequency, total ->
       case Integer.parse(frequency) do
         {number, ""} -> total + number
         :error ->
           total
+      end
+    end)
+  end
+
+  @doc """
+  iex> day_1b(["+1", "-1"])
+  0
+
+  iex> day_1b(["+3", "+3", "+4", "-2", "-4"])
+  10
+
+  iex> day_1b(["-6", "+3", "+8", "+5", "-6"])
+  5
+
+  iex> day_1b(["+7", "+7", "-2", "-7", "-4"])
+  14
+  """
+  def day_1b(frequencies) do
+    frequencies
+    |> Stream.map(fn val ->
+      case Integer.parse(val) do
+        {num, _} -> num
+        :error ->
+          raise "could not parse #{inspect val}"
+      end
+    end)
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn val, {total, totals} ->
+      total = total + val
+
+      if MapSet.member?(totals, total) do
+        {:halt, total}
+      else
+        {:cont, {total, MapSet.put(totals, total)}}
       end
     end)
   end
--- advent_elixir/test/advent_elixir_test.exs
+++ advent_elixir/test/advent_elixir_test.exs
@@ -2,13 +2,24 @@ defmodule AdventElixirTest do
   use ExUnit.Case
   doctest AdventElixir, import: true
 
-  test "day one" do
-    input =
-      Path.join([__DIR__, "./fixtures/day_1.txt"])
-      |> File.read!()
-      |> String.split("\n")
+  describe "day 1" do
+    setup do
+      fixture =
+        Path.join([__DIR__, "./fixtures/day_1.txt"])
+        |> File.read!()
+        |> String.split("\n")
+        |> Enum.reject(& String.trim(&1) == "")
 
-    assert AdventElixir.day_1(input) == 5
+      {:ok, fixture: fixture}
+    end
+
+    test "a", %{fixture: input} do
+      assert AdventElixir.day_1a(input) == 556
+    end
+
+    test "b", %{fixture: input} do
+      assert AdventElixir.day_1b(input) == 448
+    end
   end
 
 end
